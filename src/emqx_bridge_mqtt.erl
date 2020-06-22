2a2660409a4e5f20c34423fd9f19694da2270bc4
turtled <turtled@emqtt.io>
2020-06-22 19:08:26 +0800

    feature(action): Add MQTT Subscribe Resource

--- src/emqx_bridge_mqtt.erl
+++ src/emqx_bridge_mqtt.erl
@@ -120,6 +120,10 @@ send(_Conn, [], PktId) ->
     {ok, PktId};
 send(#{client_pid := ClientPid} = Conn, [Msg | Rest], _PktId) ->
     case emqtt:publish(ClientPid, Msg) of
+        ok ->
+            Ref = make_ref(),
+            self() ! {batch_ack, Ref},
+            send(Conn, Rest, Ref);
         {ok, PktId} ->
             send(Conn, Rest, PktId);
         {error, Reason} ->
